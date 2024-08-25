3296ad525929467fc5c53c3b4573c623a158bc32
Tanmay Agarwal <95555693+TanmayAgarwal123@users.noreply.github.com>
2024-08-25 12:17:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(cors());
 // Route: /bfhl | Method: GET
 app.get('/bfhl', (req, res) => {
   try {
